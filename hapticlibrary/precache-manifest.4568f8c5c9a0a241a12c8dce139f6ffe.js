fd0b33c5e6336f41c132be2ece64dac99c9ccef9
Tanay Singhal <singhal.tanay@gmail.com>
2020-05-21 16:33:20 -0400

    Added Haptic Library build v0.0.1

---
+++ hapticlibrary/precache-manifest.4568f8c5c9a0a241a12c8dce139f6ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76b9d04c1e743a63539a45785453bbf",
+    "url": "/hapticlibrary/index.html"
+  },
+  {
+    "revision": "49691945c092132d746d",
+    "url": "/hapticlibrary/static/css/main.ed6a7b05.chunk.css"
+  },
+  {
+    "revision": "f8263c5e5aa1d7bda18e",
+    "url": "/hapticlibrary/static/js/2.ab899105.chunk.js"
+  },
+  {
+    "revision": "cb169457aef1948e150074173ef51307",
+    "url": "/hapticlibrary/static/js/2.ab899105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49691945c092132d746d",
+    "url": "/hapticlibrary/static/js/main.96cf523e.chunk.js"
+  },
+  {
+    "revision": "39be5c6dbc0d509741db",
+    "url": "/hapticlibrary/static/js/runtime-main.c8506ba5.js"
+  }
+]);
---
+++ hapticlibrary/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hapticlibrary/precache-manifest.4568f8c5c9a0a241a12c8dce139f6ffe.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hapticlibrary/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
